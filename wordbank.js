f0f062152c0beee87253bfb76de881fe54700cd2
mpianca66 <mpianca66@gmail.com>
2026-05-03 13:41:18 +0200

    wordbank.js

--- wordbank.js
+++ wordbank.js
@@ -1,27 +1,66 @@
 const WORD_BANK = [
 
+  // SPORT
   {cat:"Sport", a:"Calcio", b:"Rugby"},
   {cat:"Sport", a:"Tennis", b:"Padel"},
   {cat:"Sport", a:"Basket", b:"Pallavolo"},
+  {cat:"Sport", a:"Nuoto", b:"Pallanuoto"},
+  {cat:"Sport", a:"Formula 1", b:"MotoGP"},
 
+  // ANIMALI
   {cat:"Animali", a:"Leone", b:"Tigre"},
   {cat:"Animali", a:"Cane", b:"Lupo"},
   {cat:"Animali", a:"Gatto", b:"Volpe"},
+  {cat:"Animali", a:"Mucca", b:"Bufalo"},
+  {cat:"Animali", a:"Aquila", b:"Falco"},
 
+  // CIBO
   {cat:"Cibo e Cucina", a:"Pizza", b:"Focaccia"},
   {cat:"Cibo e Cucina", a:"Pasta", b:"Riso"},
+  {cat:"Cibo e Cucina", a:"Lasagna", b:"Cannelloni"},
   {cat:"Cibo e Cucina", a:"Carbonara", b:"Amatriciana"},
+  {cat:"Cibo e Cucina", a:"Tiramisù", b:"Zuppa inglese"},
 
+  // TECNOLOGIA
   {cat:"Tecnologia", a:"Smartphone", b:"Tablet"},
   {cat:"Tecnologia", a:"Laptop", b:"Desktop"},
   {cat:"Tecnologia", a:"WiFi", b:"Bluetooth"},
+  {cat:"Tecnologia", a:"Mouse", b:"Touchpad"},
+  {cat:"Tecnologia", a:"App", b:"Sito web"},
 
+  // MUSICA
   {cat:"Musica", a:"Chitarra", b:"Basso"},
   {cat:"Musica", a:"Pianoforte", b:"Tastiera"},
   {cat:"Musica", a:"Rap", b:"Trap"},
+  {cat:"Musica", a:"Jazz", b:"Blues"},
+  {cat:"Musica", a:"Rock", b:"Metal"},
 
+  // LUOGHI
   {cat:"Luoghi", a:"Roma", b:"Milano"},
   {cat:"Luoghi", a:"Parigi", b:"Londra"},
-  {cat:"Luoghi", a:"New York", b:"Los Angeles"}
+  {cat:"Luoghi", a:"New York", b:"Los Angeles"},
+  {cat:"Luoghi", a:"Tokyo", b:"Seoul"},
+  {cat:"Luoghi", a:"Madrid", b:"Barcellona"},
+
+  // CINEMA
+  {cat:"Cinema", a:"Batman", b:"Superman"},
+  {cat:"Cinema", a:"Rocky", b:"Rambo"},
+  {cat:"Cinema", a:"Titanic", b:"Pearl Harbor"},
+  {cat:"Cinema", a:"Matrix", b:"Inception"},
+  {cat:"Cinema", a:"Jurassic Park", b:"King Kong"},
+
+  // OGGETTI
+  {cat:"Oggetti", a:"Telefono", b:"Tablet"},
+  {cat:"Oggetti", a:"Lampada", b:"Torcia"},
+  {cat:"Oggetti", a:"Sedia", b:"Poltrona"},
+  {cat:"Oggetti", a:"Forno", b:"Microonde"},
+  {cat:"Oggetti", a:"Frigorifero", b:"Congelatore"},
+
+  // PROFESSIONI
+  {cat:"Professioni", a:"Medico", b:"Infermiere"},
+  {cat:"Professioni", a:"Avvocato", b:"Notaio"},
+  {cat:"Professioni", a:"Cuoco", b:"Pasticcere"},
+  {cat:"Professioni", a:"Architetto", b:"Ingegnere"},
+  {cat:"Professioni", a:"Barista", b:"Cameriere"}
 
 ];
