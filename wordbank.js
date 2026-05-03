3b34a52465712bee02e5b7367dc6179d3ae23c7e
mpianca66 <mpianca66@gmail.com>
2026-05-03 12:25:40 +0200

    wordbank.js

--- wordbank.js
+++ wordbank.js
@@ -1,1 +1,45 @@
+const WORD_BANK = [
 
+  // SPORT
+  {cat:"Sport", a:"Calcio", b:"Rugby"},
+  {cat:"Sport", a:"Tennis", b:"Padel"},
+  {cat:"Sport", a:"Basket", b:"Pallavolo"},
+  {cat:"Sport", a:"Nuoto", b:"Pallanuoto"},
+  {cat:"Sport", a:"Formula 1", b:"MotoGP"},
+
+  // ANIMALI
+  {cat:"Animali", a:"Leone", b:"Tigre"},
+  {cat:"Animali", a:"Gatto", b:"Volpe"},
+  {cat:"Animali", a:"Cane", b:"Lupo"},
+  {cat:"Animali", a:"Mucca", b:"Bufalo"},
+  {cat:"Animali", a:"Cavallo", b:"Asino"},
+
+  // CIBO
+  {cat:"Cibo", a:"Pizza", b:"Focaccia"},
+  {cat:"Cibo", a:"Pasta", b:"Riso"},
+  {cat:"Cibo", a:"Lasagna", b:"Cannelloni"},
+  {cat:"Cibo", a:"Gelato", b:"Granita"},
+  {cat:"Cibo", a:"Pane", b:"Toast"},
+
+  // CINEMA
+  {cat:"Cinema", a:"Batman", b:"Superman"},
+  {cat:"Cinema", a:"Rocky", b:"Rambo"},
+  {cat:"Cinema", a:"Titanic", b:"Pearl Harbor"},
+  {cat:"Cinema", a:"Harry Potter", b:"Il Signore degli Anelli"},
+  {cat:"Cinema", a:"Spiderman", b:"Iron Man"},
+
+  // ANNI 80/90 (TUO MONDO 🔥)
+  {cat:"Anni 80/90", a:"Walkman", b:"Discman"},
+  {cat:"Anni 80/90", a:"VHS", b:"DVD"},
+  {cat:"Anni 80/90", a:"Game Boy", b:"Game Gear"},
+  {cat:"Anni 80/90", a:"MTV", b:"Videomusic"},
+  {cat:"Anni 80/90", a:"Bim Bum Bam", b:"Ciao Ciao"},
+
+  // OGGETTI
+  {cat:"Oggetti", a:"Telefono", b:"Tablet"},
+  {cat:"Oggetti", a:"Computer", b:"Stampante"},
+  {cat:"Oggetti", a:"Lampada", b:"Torcia"},
+  {cat:"Oggetti", a:"Orologio", b:"Sveglia"},
+  {cat:"Oggetti", a:"Sedia", b:"Poltrona"},
+
+];
